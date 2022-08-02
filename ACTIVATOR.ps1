77a4f186cb8b21edf2073fa741033fd08c885d67
Pheanoukma <103352870+Pheanoukma@users.noreply.github.com>
2022-08-02 17:49:21 +0700

    Add files via upload

---
+++ ACTIVATOR.ps1
@@ -0,0 +1,18 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/Pheanoukma/powershell-1-/main/WINDOWS-ACTIVATOR.CMD'
+
+$FilePath = "$env:TEMP\WINDOWS-ACTIVATOR.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
